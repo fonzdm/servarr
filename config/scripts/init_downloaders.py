4e6bf8c299cdeca2d3f01752729bb0fb61d58396
Giosuè Sulipano <34242779+imgios@users.noreply.github.com>
2024-04-09 20:31:20 +0200

    add init downloaders script base

---
+++ config/scripts/init_downloaders.py
@@ -0,0 +1,12 @@
+#!/usr/local/bin/python3
+
+import requests
+import os
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+log_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(log_format)
+logger.addHandler(console_handler)
