2130e4da62d47afb3cb1e1a6ab8b4d6ee6b913e3
Giosuè Sulipano <34242779+imgios@users.noreply.github.com>
2024-04-09 20:37:51 +0200

    retrieve the api key from the env

--- config/scripts/init_downloaders.py
+++ config/scripts/init_downloaders.py
@@ -10,3 +10,5 @@ console_handler = logging.StreamHandler()
 log_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 console_handler.setFormatter(log_format)
 logger.addHandler(console_handler)
+
+APIKEY = os.getenv("APIKEY")
