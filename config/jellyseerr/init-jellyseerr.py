4b8ff773d8d29bc84598cc606b2c4b7b6c279460
Giosuè Sulipano <34242779+imgios@users.noreply.github.com>
2024-04-09 12:38:31 +0200

    add python logger

--- config/jellyseerr/init-jellyseerr.py
+++ config/jellyseerr/init-jellyseerr.py
@@ -2,6 +2,14 @@
 
 import requests
 import os
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+log_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(log_format)
+logger.addHandler(console_handler)
 
 APIKEY = os.getenv("APIKEY")
 JELLYFIN_USERNAME = os.getenv("JELLYFIN_USERNAME")
