1971d22e86eea6ef53886e1b665772526ce3935a
Giosuè Sulipano <34242779+imgios@users.noreply.github.com>
2024-04-12 18:41:51 +0200

    add qbittorrent into sonarr config

--- servarr/config/scripts/init_sonarr.py
+++ servarr/config/scripts/init_sonarr.py
@@ -12,8 +12,11 @@ log_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messa
 console_handler.setFormatter(log_format)
 logger.addHandler(console_handler)
 
-SERVARR_HOST = os.getenv("SERVARR_HOST")
+SONARR_HOST = os.getenv("SONARR_HOST")
 API_KEY = os.getenv("API_KEY")
+TORRENT_USERNAME = os.getenv("TORRENT_USERNAME")
+TORRENT_PASSWORD = os.getenv("TORRENT_PASSWORD")
+TORRENT_HOST = os.getenv("TORRENT_HOST", "servarr-qbittorrent")
 
 def post(url: str, headers: dict, body: dict):
     """
@@ -55,3 +58,88 @@ def post(url: str, headers: dict, body: dict):
     except JSONDecodeError:
         return {"code": response.status_code, "response": response.text}
 
+logger.info("Setup Sonarr and qBitTorrent interworking")
+
+headers = {
+    "content-type": "application/json",
+    "x-api-key": API_KEY,
+    "x-requested-with": "XMLHttpRequest"
+}
+
+body = {
+    "enable": true,
+    "protocol": "torrent",
+    "priority": 1,
+    "removeCompletedDownloads": true,
+    "removeFailedDownloads": true,
+    "name": "qBittorrent",
+    "fields": [
+        {
+            "name": "host",
+            "value": TORRENT_HOST
+        },
+        {
+            "name": "port",
+            "value": "10095"
+        },
+        {
+            "name": "useSsl",
+            "value": false
+        },
+        {
+            "name": "urlBase"
+        },
+        {
+            "name": "username",
+            "value": TORRENT_USERNAME
+        },
+        {
+            "name": "password",
+            "value": TORRENT_PASSWORD
+        },
+        {
+            "name": "movieCategory",
+            "value": "radarr"
+        },
+        {
+            "name": "movieImportedCategory"
+        },
+        {
+            "name": "recentMoviePriority",
+            "value": 0
+        },
+        {
+            "name": "olderMoviePriority",
+            "value": 0
+        },
+        {
+            "name": "initialState",
+            "value": 0
+        },
+        {
+            "name": "sequentialOrder",
+            "value": false
+        },
+        {
+            "name": "firstAndLast",
+            "value": false
+        },
+        {
+            "name": "contentLayout",
+            "value": 0
+        }
+    ],
+    "implementationName": "qBittorrent",
+    "implementation": "QBittorrent",
+    "configContract": "QBittorrentSettings",
+    "infoLink": "https://wiki.servarr.com/radarr/supported#qbittorrent",
+    "tags": []
+}
+
+res = post(
+    url="http://{}/api/v3/downloadclient".format(SONARR_HOST),
+    headers=headers,
+    body=body
+)
+
+# TO-DO: Check for response status code and decide what to do
