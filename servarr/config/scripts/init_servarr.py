e43f7e84e68af00a1f6bdf54e715401ed5f6ea57
Giosuè Sulipano <34242779+imgios@users.noreply.github.com>
2024-04-12 18:30:56 +0200

    create servarr init script

---
+++ servarr/config/scripts/init_servarr.py
@@ -0,0 +1,13 @@
+#!/usr/local/bin/python3
+
+import requests
+import os
+import logging
+from json import JSONDecodeError
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+log_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(log_format)
+logger.addHandler(console_handler)
