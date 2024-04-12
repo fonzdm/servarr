5e76c89c9c67b29f3ba197422e2adc0af8e8dbc3
Giosuè Sulipano <34242779+imgios@users.noreply.github.com>
2024-04-12 18:33:15 +0200

    add post function

--- servarr/config/scripts/init_servarr.py
+++ servarr/config/scripts/init_servarr.py
@@ -14,3 +14,43 @@ logger.addHandler(console_handler)
 
 SERVARR_HOST = os.getenv("SERVARR_HOST")
 API_KEY = os.getenv("API_KEY")
+
+def post(url: str, headers: dict, body: dict):
+    """
+    Handle POST requests towards an url, logging both
+    the details before sending it and the response.
+
+    Parameters
+    ----------
+    url : str
+        HTTP request URL as string
+    headers : dict
+        HTTP headers to be used in the request
+    body : dict
+        Request body needed for the POST
+    """
+
+    logger.debug(" ".join([
+        "POST",
+        url,
+        ", ".join([": ".join(header) for header in headers]),
+        str(body)
+    ]))
+
+    response = requests.post(
+        url=url,
+        data=body,
+        headers=headers
+    )
+
+    logger.debug(" ".join([
+        "Status Code:",
+        response.status_code,
+        "Response body:",
+        response.text
+    ]))
+
+    try:
+        return {"code": response.status_code, "response": response.json()}
+    except JSONDecodeError:
+        return {"code": response.status_code, "response": response.text}
